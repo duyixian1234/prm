1b8e208f47fdd6fc38b5f5a2d633c51e1a932fa7
duyixian <duyixian1234@qq.com>
2021-04-29 16:57:14 +0800

    lint with black

--- prm/prm.py
+++ prm/prm.py
@@ -4,29 +4,25 @@ import pathlib
 import click
 
 repositories = [
+    {"name": "pypi", "url": "https://pypi.org/simple", "trusted-host": "pypi.org"},
     {
-        'name': 'pypi',
-        'url': 'https://pypi.org/simple',
-        'trusted-host': 'pypi.org'
+        "name": "douban",
+        "url": "https://pypi.douban.com/simple",
+        "trusted-host": "pypi.douban.com",
     },
     {
-        'name': 'douban',
-        'url': 'https://pypi.douban.com/simple',
-        'trusted-host': 'pypi.douban.com'
+        "name": "tencent",
+        "url": "https://mirrors.cloud.tencent.com/pypi/simple",
+        "trusted-host": "mirrors.cloud.tencent.com",
     },
     {
-        'name': 'tencent',
-        'url': 'https://mirrors.cloud.tencent.com/pypi/simple',
-        'trusted-host': 'mirrors.cloud.tencent.com'
-    },
-    {
-        'name': 'aliyun',
-        'url': 'https://mirrors.aliyun.com/pypi/simple/',
-        'trusted-host': 'mirrors.aliyun.com'
+        "name": "aliyun",
+        "url": "https://mirrors.aliyun.com/pypi/simple/",
+        "trusted-host": "mirrors.aliyun.com",
     },
 ]
 
-pip = pathlib.Path.home() / pathlib.Path('.pip')
+pip = pathlib.Path.home() / pathlib.Path(".pip")
 
 
 @click.group()
@@ -35,20 +31,23 @@ def cli():
 
 
 @click.command()
-@click.argument('repository')
+@click.argument("repository")
 def use(repository: str):
     for one in repositories:
-        if one['name'] == repository:
-            print(f'Setting to {repository}')
+        if one["name"] == repository:
+            print(f"Setting to {repository}")
             config = configparser.ConfigParser()
-            config['global'] = {'index-url':one['url'], 'trusted-host':one['trusted-host']}
+            config["global"] = {
+                "index-url": one["url"],
+                "trusted-host": one["trusted-host"],
+            }
             if not pip.exists():
                 pip.mkdir()
-            with open(pip.as_posix() + 'pip.conf','w') as file:
+            with open(pip.as_posix() + "pip.conf", "w") as file:
                 config.write(file)
             return
     else:
-        print(f'No repository {repository}')
+        print(f"No repository {repository}")
 
 
 @click.command()
@@ -56,14 +55,16 @@ def list():
     for repository in repositories:
         print(f'{repository["name"]:20}{repository["url"]}\n')
 
+
 @click.command()
 def show():
     if pip.exists():
         config = configparser.ConfigParser()
-        config.read(pip.as_posix() + 'pip.conf')
+        config.read(pip.as_posix() + "pip.conf")
         print(f'Current: {config["global"]["index-url"]}')
     else:
-        print('{:20}{}'.format(repositories[0]['name'], repositories[0]['url']))
+        print("{:20}{}".format(repositories[0]["name"], repositories[0]["url"]))
+
 
 def main():
     cli.add_command(list)
@@ -71,5 +72,6 @@ def main():
     cli.add_command(show)
     cli()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
